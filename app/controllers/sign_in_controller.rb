775b99c7422cb7fe5e1063629bdda9773510150e
Daniel Hudy <djh3315@rit.edu>
2015-03-05 21:59:28 -0500

    added user model and more fb auth stuff

--- app/controllers/sign_in_controller.rb
+++ app/controllers/sign_in_controller.rb
@@ -1,5 +1,13 @@
 class SignInController < ApplicationController
-  def index
-    
+  def create
+    # @user = User.find_or_create_from_auth_hash(auth_hash)
+    self.current_user = @user
+    redirect_to '/dashing/dashboards'
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root '/auth/facebook'
+  root 'auth/facebook', action: 'sign_in#index'
+  get 'auth/facebook/callback', to: 'sign_in#create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
