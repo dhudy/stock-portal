1418ca4a02a9984f02cd1fb393f4a16c64998426
Daniel Hudy <djh3315@rit.edu>
2015-03-06 00:02:22 -0500

    Fixing csrf error

--- app/controllers/sign_in_controller.rb
+++ app/controllers/sign_in_controller.rb
@@ -1,16 +1,5 @@
 class SignInController < ApplicationController
   before_action :authenticate_user!
-  def create
-    # raise auth_hash.inspect
-    @user = User.where(email: auth_hash[:info][:email]).first_or_create
-    @user.auth_token = auth_hash[:credentials][:token]
-    self.current_user = @user
-    redirect_to '/dashing/dashboards'
-  end
-
-  def failure
-    raise params[:message].inspect
-  end
 
   protected
 
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    puts "--#{request.env['omniauth.auth'].inspect}"
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       sign_in @user
