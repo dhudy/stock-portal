69a61fc7566fe3331a865727fc156b5a72a07961
Daniel Hudy <djh3315@rit.edu>
2015-03-06 21:27:24 -0500

    trying to fix data post

--- app/controllers/facebook/facebook_controller.rb
+++ app/controllers/facebook/facebook_controller.rb
@@ -2,6 +2,6 @@ class FacebookController < ApplicationController
   def post
     #Handle posting the post to facebook
     puts "Retrieved the post request"
-    redirect_to '/dashing/dashboards'
+    
   end
 end
