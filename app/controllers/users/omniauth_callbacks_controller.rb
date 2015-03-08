993e8c21e12681a5caff1c4f445d134d879eee5f
Daniel Hudy <djh3315@rit.edu>
2015-03-08 15:49:43 -0400

    The job is done... RIP auth token

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,8 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    puts "--#{request.env['omniauth.auth']['credentials']['token'].inspect}"
+    # puts "--#{request.env['omniauth.auth']['credentials']['token'].inspect}"
     @user = User.from_omniauth(request.env['omniauth.auth'])
-
+    @user.auth_token = request.env['omniauth.auth']['credentials']['token']
     if @user.persisted?
       sign_in @user
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
