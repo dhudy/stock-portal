304affae81b07479e93440abaa8641447b42ab0b
Daniel Hudy <djh3315@rit.edu>
2015-03-08 15:48:07 -0400

    CANT HIDE NOW

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    puts "--#{request.env['omniauth.auth']['credentials'].inspect}"
+    puts "--#{request.env['omniauth.auth']['credentials']['token'].inspect}"
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
