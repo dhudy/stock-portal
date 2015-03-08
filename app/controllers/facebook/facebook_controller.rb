38de94401aade57f25c349be7af600e5f6cb8e83
Daniel Hudy <djh3315@rit.edu>
2015-03-08 18:32:11 -0400

    Not sure if i can get any details other than my basic user data...

--- app/controllers/facebook/facebook_controller.rb
+++ app/controllers/facebook/facebook_controller.rb
@@ -8,6 +8,8 @@ class Facebook::FacebookController < ApplicationController
 
   def feed
     @graph = Koala::Facebook::API.new(current_user.auth_token)
+    myself = @graph.get_object("me")
+    puts "$$$$$ #{myself.inspect}"
     profile = @graph.get_connections("me", "friends")
     profile.each do |friend|
       puts "$$$$$ #{friend.inspect}"
