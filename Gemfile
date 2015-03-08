4a3952158eef19e3bbfcf53646a4f46f0f7fea8d
Daniel Hudy <djh3315@rit.edu>
2015-03-08 19:30:44 -0400

    Added bootstrap

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'dashing-rails'
 gem 'omniauth-facebook', '1.5.1'
 gem 'devise'
 gem "koala"
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/facebook/facebook_controller.rb
+++ app/controllers/facebook/facebook_controller.rb
@@ -3,7 +3,7 @@ class Facebook::FacebookController < ApplicationController
   def post
     #Handle posting the post to facebook
     @graph = Koala::Facebook::API.new(current_user.auth_token)
-    @graph.put_connections("me", "feed", :message => params[:fpost])
+    # @graph.put_connections("me", "feed", :message => params[:fpost])
     redirect_to '/dashing/dashboards'
   end
 
@@ -11,8 +11,8 @@ class Facebook::FacebookController < ApplicationController
     @graph = Koala::Facebook::API.new(current_user.auth_token)
     myself = @graph.get_object("me")
     # puts "$$$$$ #{myself.inspect}"
-    profile = @graph.get_connections("me", "home")
-    puts "$$$$$ #{profile.inspect}"
+    # profile = @graph.get_connections("me", "home")
+    # puts "$$$$$ #{profile.inspect}"
     # profile.each do |friend|
     #   puts "$$$$$ #{friend.inspect}"
     # end
